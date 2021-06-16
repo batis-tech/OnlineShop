091bfbd35c0ac5bf0fc12ffebfffe3b16db99118
hasan <hasanbatis440@gmail.com>
2021-06-16 16:23:50 +0300

    finishing add to MVP ecoommerce within 1 hour

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :products
   resources :shops, only:[:index, :show]
+  resources :order_items
+  resource :cards, only:[:show] 
   root "shops#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
